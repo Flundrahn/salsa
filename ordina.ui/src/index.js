3781576f6e2d8022c1ec17bed633f24582676a66
Fredrik Lundström <flundst@gmail.com>
2022-07-18 12:01:53 +0200

    add react router

--- ordina.ui/src/index.js
+++ ordina.ui/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from './App';
 import { WeeksProvider } from './components/WeekContext';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <WeeksProvider>
-    <App />
-  </WeeksProvider>
+    <BrowserRouter>
+      <WeeksProvider>
+        <App />
+      </WeeksProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
