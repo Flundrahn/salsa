261404671572e74cd8a61302e841e033cb4920f2
Fredrik Lundström <flundst@gmail.com>
2022-07-20 08:55:21 +0200

    update styling

--- ordina.ui/src/components/Timeline.js
+++ ordina.ui/src/components/Timeline.js
@@ -9,37 +9,37 @@ function Timeline() {
   return (
     <div className="timeline">
       {
-    React.Children.toArray(
-      weeks.map(w => (
-        <div className="week__container">
-          <div className="week__header">
-            <span className="timeline__date week__date">{`Week ${w.weekNumber}`}</span>
-            <div className="timeline__line" />
-            <div className="topic__bullet" />
-            <h3 className="timeline__title week__title">{w.title}</h3>
-          </div>
-          <div className="week__body">
-            {
-            React.Children.toArray(
-              w.topics.map(t => (
-                <div className="topic__container">
-                  <span className="timeline__date topic__date">{`Day ${t.day}`}</span>
-                  <div className="timeline__line" />
-                  <div className="topic__bullet" />
-                  <Link
-                    to={`/topic/${t.topicId}`}
-                    className="timeline__title topic__title">
-                    {' '}
-                    {t.title}
-                  </Link>
-                </div>
-              )),
-            )
-          }
-          </div>
-        </div>
-      )),
-    )
+        React.Children.toArray(
+          weeks.map(w => (
+            <div className="week__container">
+              <div className="timeline__row">
+                <span className="timeline__date week__date">{`Week ${w.weekNumber}`}</span>
+                <div className="timeline__line" />
+                <div className="week__bullet" />
+                <h3 className="timeline__title week__title">{w.title}</h3>
+              </div>
+              {/* <div className="week__body"> */}
+              {
+                React.Children.toArray(
+                  w.topics.map(t => (
+                    <div className="timeline__row">
+                      <span className="timeline__date topic__date">{`Day ${t.day}`}</span>
+                      <div className="timeline__line" />
+                      <div className="topic__bullet" />
+                      <Link
+                        to={`/topic/${t.topicId}`}
+                        className="timeline__title topic__title">
+                        {' '}
+                        {t.title}
+                      </Link>
+                    </div>
+                  )),
+                )
+              }
+              {/* </div> */}
+            </div>
+          )),
+        )
       }
     </div>
   );
