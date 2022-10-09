aa46872a474cbf5682397237d8fd330a2e230747
Flundrahn <flundst@gmail.com>
2022-10-09 22:15:31 +0200

    preload background & fonts

--- salsa.ui/src/index.js
+++ salsa.ui/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import { ValueProvider } from './components/ValueContext';
 import { AuthProvider } from './components/AuthContext';
+import '@fontsource/raleway';
+import '@fontsource/roboto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
