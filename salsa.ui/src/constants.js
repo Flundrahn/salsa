f2bf0644574c32920b218aa658d526253659e07a
Flundrahn <flundst@gmail.com>
2022-10-09 21:27:53 +0200

    fix url mistake

--- salsa.ui/src/constants.js
+++ salsa.ui/src/constants.js
@@ -1,7 +1,7 @@
 const RESOURCE_TYPES = ['Lab', 'Slide', 'Cheatsheet', 'Article', 'Video', 'Weekend Test'];
 
 const API_URL = process.env.NODE_ENV === 'development'
-  ? 'https://salsa-api.azurewebsites.net/api'
-  : 'https://localhost:7053/api';
+  ? 'https://localhost:7053/api'
+  : 'https://salsa-api.azurewebsites.net/api';
 
 export { RESOURCE_TYPES, API_URL };
